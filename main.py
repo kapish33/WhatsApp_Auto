bc3d78e52b2b7a36a1f717b4d7780c7bd17685f0
kapish33 <59023093+kapish33@users.noreply.github.com>
2021-04-30 14:22:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pywhatkit
 
-from contactList import contact_Names,contact_Number
+from contactList import contact_number, contact_names
 from message import myMessage
 
-hour=int(input("Kitna ghnata ma Bheuu :"))
-minute=int(input("Kitna Minute pa Bhezuu  :"))
+hour = int(input("Kitna Baza "))
+hour=hour%24
+minute = int(input("Kitna Minute Pa"))
+minute= minute%60
 
-
-times=len(contact_Names)
+times = len(contact_names)
 
 for cn in range(times):
-    cno="+91"+f"{contact_Number[cn]}"
+
+    cno="+91"+f"{contact_number[cn]}"
     message=str(myMessage(cn))
 
-    pywhatkit.sendwhatmsg(cno,message,hour,minute)
-    minute=minute+1/2
+    pywhatkit.sendwhatmsg(cno, message, hour, minute)
+    minute=minute+0.5
+    if minute>=59:
+        minute=1
+        hour=hour+1
+
+
